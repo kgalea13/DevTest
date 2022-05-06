15236914b4a25d9e754ff922e30c1ed8f481e9d9
Kristin_Galea <kgalea13@gmail.com>
2022-05-06 10:05:32 -0400

    init

--- Basic-JS/index.js
+++ Basic-JS/index.js
@@ -5,10 +5,9 @@
 // Below are varriables to be used for the below questions
 // Global Varriables
 
-var departmentInfo = require('./json/departmentInfo.json');
+var departmentInfo = require('./json/departmentInfo.json'); 
 var objectArray = require('./json/objectArray.json');
 var people = require('./json/people.json');
-
 var objectF = {
   value: 'f',
   Id: 5
@@ -18,14 +17,31 @@ var objectF = {
 // end Global Varriables 
 
 // 1. Write a function that adds two given numbers
-
+const addsTwo = (a,b) => a + b;
 
 // 2. Adding 'objectF' to an 'objectArray'
-
+objectArray.push(objectF);
 
 // 3. Write a function that returns Id 3 from 'objectArray'
-
+const returnsThree = () => {
+  return objectArray[2]["ID"];
+};
 
 // 4. Write a function that merges 'people' with 'departmentInfo' based on department
+ const departmentMerge = () => {
+    forEach(people => {
+      for(peoplesDept in people){
+        if(people.includes('TechOps')){
+          people.push(departmentInfo[1].department);
+        }else if(people.includes('Reliability')){
+          people.push(departmentInfo[2].department);
+        }
+      }
+    })
+  };  
 
-
+//Loop through the array stored in variable 'people' with forEach()
+//Loop through the object within the array using a for...in loop
+//Use a conditional to evaluate if 'techOps' is in the object
+//If 'techOps' IS in the object, push() it into the departmentInfo array
+//If instead the object contains 'Reliability', then push() into the object which contains 'Reliability'
--- Basic-TS/index.ts
+++ Basic-TS/index.ts
@@ -1,9 +1,68 @@
 // - Create interface for the workload json
+interface IWorkload {
+
+        Nose: string;
+        Station?: string;
+        ATA: number;
+        BOWTYPE: string;
+        COMMENTIND: string;
+        Priority: string;
+        Carrier: string;
+        CRITUPGRADE: boolean;
+        Created: string;
+        EMPLOYEENUMBER: string;
+        FLEET: string;
+        IDENTIFY: string;
+        ID: number;
+        MEL: string;
+        MENUMBER: string;
+        Modified: string;
+        REF: any;
+        TASKDESCRIPTION: string;
+        TOTALHRS: number;
+        CRIT: number;
+        partsData: any[];
+}       
 
 // - Create types
 
+type Workload =  {
+     Nose: string;
+      ATA: string;
+      BOWTYPE: string;
+      COMMENTIND: string;
+      Priority: string;
+      Carrier: string;
+      CRITUPGRADE: false;
+      Created: string;
+      EMPLOYEENUMBER: string;
+      FLEET: string;
+      IDENTIFY: string;
+      ID: number;
+      MEL: string;
+      MENUMBER: string;
+      Modified: string;
+      REF: any;
+      TASKDESCRIPTION: string;
+      TOTALHRS: number;
+      CRIT: number;
+      partsData: any[];
+    }
+
 // - Create simple person class, that accepts the persons first name
 
-// - Create a Mecahnic class that exstends the above person class and has a field 'title' set to 'AMT'
+class Person {
+    constructor(firstName){
+        this._firstName;
+    }
 
-// - 
+}
+
+// - Create a Mecahnic class that exstends the above person class and has a field 'title' set to 'AMT'
+class Mecahnic extends Person {
+    constructor(AMT){
+        super(firstName)
+        this._title = AMT;
+    }
+}
+
